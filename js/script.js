f82a056e1d2efbbd4fca3c68084424b2b67fdb7d
Gerard <gerard.g.johnson@gmail.com>
2023-04-29 16:16:25 -0500

    added appends and elements ul and div

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 let stores = [
-    { store: "Seattle", minCust: 23, maxCust: 65, avgCookiesPerSale: 6.3, randCustThisHour: getRandomInt},
+    { store: "Seattle", minCust: 23, maxCust: 65, avgCookiesPerSale: 6.3, },
     { store: "Tokyo", minCust: 3, maxCust: 24, avgCookiesPerSale: 1.2 },
     { store: "Dubai", minCust: 11, maxCust: 38, avgCookiesPerSale: 3.7 },
     { store: "Paris", minCust: 20, maxCust: 38, avgCookiesPerSale: 2.3 },
@@ -21,14 +21,46 @@ function getRandomInt(min, max) {
   
 // Loop through the stores and add a randCustPerHour key/value to each other
 for (let store of stores) {
-    store['avgCookiesPurchased'] = getRandomInt(store['minCust'], store['maxCust']);
-    console.log(store)
-}
+    store['avgCookiesPurchased'] = getRandomInt(
+        store['minCust'],
+        store['maxCust']
+    );
+    // console.log(store);
 
-for(let store of stores) {
-    for (let hour of hours) 
-        store["amountPurchasedPerHour"] = []
-    store["amountPurchasedPerHour"].push(
-        store["averageCookiesPurchased"] * store["avgCookiesPerSale"]):
+
+    for (let store of stores) {
+        store["amountPurchasedPerHour"] = [];
+
+        for (let hour of hours)
+        
+            // console.log(store["amountPurchasedPerHour"])
+        
+            store["amountPurchasedPerHour"].push(
+             Math.floor (store["avgCookiesPurchased"] * store["avgCookiesPerSale"])
+            );
+    }
 }
-console.log(store);
+console.log(stores);
+
+let seattleDivElement = document.getElementById("seattle")
+let seattleUlElement = document.createElement("ul")
+seattleDivElement.appendChild(seattleUlElement)
+
+let tokyoDivElement = document.getElementById("tokyo")
+let tokyoUlElement = document.createElement("ul")
+tokyoDivElement.appendChild(tokyoUlElement)
+
+let dubaiDivElement = document.getElementById("dubai")
+let dubaiUlElement = document.createElement("ul")
+dubaiDivElement.appendChild(dubaiUlElement)
+
+
+let parisDivElement = document.getElementById("paris")
+let parisUlElement = document.createElement("ul")
+parisDivElement.appendChild(parisUlElement)
+
+let limaDivElement = document.getElementById("lima")
+let limaUlElement = document.createElement("ul")
+limaDivElement.appendChild(limaUlElement)
+
+
