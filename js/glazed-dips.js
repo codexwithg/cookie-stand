f6acef4395e190c8a3c6a59f3c15d94dd8932108
Gerard <gerard.g.johnson@gmail.com>
2023-06-11 18:15:19 -0500

    small changes for check

--- js/glazed-dips.js
+++ js/glazed-dips.js
@@ -2,20 +2,22 @@
 let numDonuts = prompt("How many donuts do you want to purchase?");
 
 // Discount code input
-let discountCode = prompt("Have a discount code? Enter 'GDD10' for 10% off or 'GGD50' for $5 off.");
+let discountCode = prompt(
+  "Have a discount code? Enter 'GDD10' for 10% off or 'GGD50' for $5 off."
+);
 
 // Prices for donuts, tax, shipping, etc.
-let pricePerDonut = 1.50;
+let pricePerDonut = 1.5;
 let taxRate = 0.052;
 let shippingCost = 3.99;
 
 // calculate subtotal
-let subtotal = numDonuts * pricePerDonut
+let subtotal = numDonuts * pricePerDonut;
 
 // apply discount code
 let discount = 0.0;
 if (discountCode === "10FF") {
-    discount = subtotal * 0.1;
+  discount = subtotal * 0.1;
 } else if (discountCode === "50FF") {
-    discount = 5.0;
+  discount = 5.0;
 }
